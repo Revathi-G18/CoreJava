0bcf25e5f11828f215af087d572d1e8aae664b5e
Revathi-G18 <36435402+Revathi-G18@users.noreply.github.com>
2018-05-26 08:03:25 +0530

    Update PalindromString.java

--- src/com/core/java/pgm/PalindromString.java
+++ src/com/core/java/pgm/PalindromString.java
@@ -3,7 +3,7 @@ package com.core.java.pgm;
 public class PalindromString {
 public boolean isStringPalindrom(String s){
 	//why reverse variable?
-	String reverse="";
+	//String reverse="";
 	int length=s.length();
 	for(int i=0;i<length/2;i++){
 	if(s.charAt(i)!=s.charAt(s.length()-i-1)){
